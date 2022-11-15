71c3ee1c5218cc6a091b60162f0c377f2a0ac47b
James Leakos <jamesleakos@gmail.com>
2022-11-15 13:00:15 -0700

    small push

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -3,80 +3,80 @@
  * Promisify them if you can, otherwise roll your own promise returning function
  */
 
- var fs = require('fs');
- var request = require('needle');
- var crypto = require('crypto');
- var Promise = require('bluebird');
+var fs = require('fs');
+var request = require('needle');
+var crypto = require('crypto');
+var Promise = require('bluebird');
 
- // (1) Asyncronous HTTP request
- var getGitHubProfile = function (user, callback) {
-   var url = 'https://api.github.com/users/' + user;
-   var options = {
-     headers: { 'User-Agent': 'request' },
-   };
+// (1) Asyncronous HTTP request
+var getGitHubProfile = function (user, callback) {
+  var url = 'https://api.github.com/users/' + user;
+  var options = {
+    headers: { 'User-Agent': 'request' },
+  };
 
-   request.get(url, options, function (err, res, body) {
-     if (err) {
-       callback(err, null);
-     } else if (body.message) {
-       callback(
-         new Error('Failed to get GitHub profile: ' + body.message),
-         null
-       );
-     } else {
-       callback(null, body);
-     }
-   });
- };
+  request.get(url, options, function (err, res, body) {
+    if (err) {
+      callback(err, null);
+    } else if (body.message) {
+      callback(
+        new Error('Failed to get GitHub profile: ' + body.message),
+        null
+      );
+    } else {
+      callback(null, body);
+    }
+  });
+};
 
- var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
- // (2) Asyncronous token generation
- var generateRandomToken = function(callback) {
-   crypto.randomBytes(20, function(err, buffer) {
-     if (err) { return callback(err, null); }
-     callback(null, buffer.toString('hex'));
-   });
- };
+// (2) Asyncronous token generation
+var generateRandomToken = function(callback) {
+  crypto.randomBytes(20, function(err, buffer) {
+    if (err) { return callback(err, null); }
+    callback(null, buffer.toString('hex'));
+  });
+};
 
- var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
- // (3) Asyncronous file manipulation
- var readFileAndMakeItFunny = function(filePath, callback) {
-   fs.readFile(filePath, 'utf8', function(err, file) {
-     if (err) { return callback(err); }
+// (3) Asyncronous file manipulation
+var readFileAndMakeItFunny = function(filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, file) {
+    if (err) { return callback(err); }
 
-     var funnyFile = file.split('\n')
-       .map(function(line) {
-         return line + ' lol';
-       })
-       .join('\n');
+    var funnyFile = file.split('\n')
+      .map(function(line) {
+        return line + ' lol';
+      })
+      .join('\n');
 
-     callback(funnyFile);
-   });
- };
+    callback(funnyFile);
+  });
+};
 
- var readFileAndMakeItFunnyAsync = function(filePath) {
-   return new Promise((resolve, reject) => {
-     fs.readFile(filePath, 'utf8', (err, file) => {
-       if (err) {
-         reject(err);
-       } else {
-         var funnyFile = file.split('\n')
-           .map(function(line) {
-             return line + ' lol';
-           })
-           .join('\n');
+var readFileAndMakeItFunnyAsync = function(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, file) => {
+      if (err) {
+        reject(err);
+      } else {
+        var funnyFile = file.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
 
-         resolve(funnyFile);
-       }
-     });
-   });
- };
+        resolve(funnyFile);
+      }
+    });
+  });
+};
 
- // Export these functions so we can test them and reuse them in later exercises
- module.exports = {
-   getGitHubProfileAsync: getGitHubProfileAsync,
-   generateRandomTokenAsync: generateRandomTokenAsync,
-   readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
- };
+// Export these functions so we can test them and reuse them in later exercises
+module.exports = {
+  getGitHubProfileAsync: getGitHubProfileAsync,
+  generateRandomTokenAsync: generateRandomTokenAsync,
+  readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
+};
